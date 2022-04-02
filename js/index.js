050deb6c678ea6c766886845a4b54ac658c7a903
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-04-02 15:31:42 +0300

    create first step(window) quiz

--- js/index.js
+++ js/index.js
@@ -15,6 +15,11 @@ let index = 0;
 // Прогресс
 const progressSuccess = document.querySelector('.progress-success');
 
+// Первый шаг
+const firstStepCard = document.querySelectorAll('.first-step-card');
+const typeDesign = document.querySelectorAll('.type-design');
+const formQuizItem = document.querySelectorAll('.form-quiz-item');
+
 
 
 btnStart.addEventListener('click', function () {
@@ -22,7 +27,10 @@ btnStart.addEventListener('click', function () {
   formQuiz.classList.toggle('visible');
   progressBarItems[index].classList.add('active-progress')
   progressBarImage[index].classList.add('active-progress__img');
+  formQuizItem[index].classList.add('step-visible');
   index = 1;
+  btnNext.disabled = true;
+  btnNext.style.opacity = '.5'
 });
 
 
@@ -32,7 +40,10 @@ btnNext.addEventListener('click', function () {
   } else {
     progressBarItems[index].classList.add('active-progress');
     progressBarImage[index].classList.add('active-progress__img');
+    formQuizItem[index].classList.add('step-visible');
+    formQuizItem[index - 1].classList.remove('step-visible');
     index += 1;
+
   }
   const activeProgress = document.querySelectorAll('.active-progress');
   const persentNext = ((activeProgress.length - 1) / (progressBarItems.length - 1)) * 100 + '%';
@@ -53,5 +64,38 @@ btnPrev.addEventListener('click', function () {
     progressBarImage[index].classList.remove('active-progress__img');
   }
 
+  formQuizItem[index - 1].classList.add('step-visible');
+  formQuizItem[index].classList.remove('step-visible');
+
+  firstStepCard[0].style.background = '#2A2A2A'
+  firstStepCard[1].style.background = '#2A2A2A'
+
+});
+
+
+firstStepCard[0].addEventListener('click', function () {
+  typeDesign[0].checked = 'checked';
+  firstStepCard[0].style.background = '#171717'
+  firstStepCard[1].style.background = '#2A2A2A'
+  firstStepCard[1].style.transitionDuratio = '0.4s';
+  firstStepCard[0].style.transitionDuratio = '0.4s';
+
+  if (typeDesign[0].checked === true) {
+    btnNext.disabled = false;
+    btnNext.style.opacity = '1';
+  }
+
+});
+
+firstStepCard[1].addEventListener('click', function () {
+  typeDesign[1].checked = 'checked';
+  firstStepCard[1].style.background = '#171717'
+  firstStepCard[0].style.background = '#2A2A2A'
+  firstStepCard[0].style.transitionDuration = '0.4s';
+  firstStepCard[1].style.transitionDuration = '0.4s';
+  if (typeDesign[1].checked === true) {
+    btnNext.disabled = false;
+    btnNext.style.opacity = '1';
+  }
 });
 
