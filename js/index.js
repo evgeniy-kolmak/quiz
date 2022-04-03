c81fa98fdbc51c012379b5a23ae414698d731af9
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-04-03 10:57:43 +0300

    create second step(window) quiz

--- js/index.js
+++ js/index.js
@@ -20,6 +20,10 @@ const firstStepCard = document.querySelectorAll('.first-step-card');
 const typeDesign = document.querySelectorAll('.type-design');
 const formQuizItem = document.querySelectorAll('.form-quiz-item');
 
+// Второй шаг
+const budgetSlider = document.querySelector('.budget-content__slider');
+const budgetOutput = document.querySelector('.budget-content__output');
+
 
 
 btnStart.addEventListener('click', function () {
@@ -49,6 +53,8 @@ btnNext.addEventListener('click', function () {
   const persentNext = ((activeProgress.length - 1) / (progressBarItems.length - 1)) * 100 + '%';
   progressSuccess.style.width = persentNext;
 
+  btnNext.disabled = true;
+  btnNext.style.opacity = '.5';
 });
 
 btnPrev.addEventListener('click', function () {
@@ -66,9 +72,10 @@ btnPrev.addEventListener('click', function () {
 
   formQuizItem[index - 1].classList.add('step-visible');
   formQuizItem[index].classList.remove('step-visible');
-
   firstStepCard[0].style.background = '#2A2A2A'
   firstStepCard[1].style.background = '#2A2A2A'
+  btnNext.disabled = true;
+  btnNext.style.opacity = '.5';
 
 });
 
@@ -79,11 +86,11 @@ firstStepCard[0].addEventListener('click', function () {
   firstStepCard[1].style.background = '#2A2A2A'
   firstStepCard[1].style.transitionDuratio = '0.4s';
   firstStepCard[0].style.transitionDuratio = '0.4s';
-
   if (typeDesign[0].checked === true) {
     btnNext.disabled = false;
     btnNext.style.opacity = '1';
   }
+
 
 });
 
@@ -97,5 +104,14 @@ firstStepCard[1].addEventListener('click', function () {
     btnNext.disabled = false;
     btnNext.style.opacity = '1';
   }
+
 });
 
+
+budgetSlider.addEventListener('input', function () {
+  budgetOutput.textContent = budgetSlider.value + '$';
+  if (budgetSlider.value > '1550' || budgetSlider.value < '1550') {
+    btnNext.disabled = false
+    btnNext.style.opacity = '1';
+  }
+})
